98533fbc1ce0fb1c125fc62ad07b78456f7439b0
Henrik Næss <henrik@heka.no>
2022-03-23 15:48:01 +0100

    fix for safari

--- app.js
+++ app.js
@@ -6,19 +6,6 @@ for (let tabGroup of tabGroups) {
 	// Get all the li elements in the tab group.
 	let lis = tabGroup.querySelectorAll('li');
 
-	// Function to hide all the content, and deactivate all the tabs.
-	function hideAll() {
-		for (let li of lis) {
-
-			// Remove the 'active' class from the li element so it doesn't have the black line.
-			li.classList.remove('active');
-
-			// Get all the divs with the same data-tag as the li element, and add the 'hidden' class.
-			tabGroup.querySelectorAll(`div[data-tab='${li.dataset.tab}']`)
-				.forEach(el => el.classList.add('hidden'));
-		}
-	}
-
 	// Loop through the li elements inside the tab group.
 	for (let li of lis) {
 		// Get the anchor inside the li element.
@@ -28,7 +15,7 @@ for (let tabGroup of tabGroups) {
 		a.addEventListener('click', () => {
 
 			// Hide all the tabs.
-			hideAll();
+			hideAll(tabGroup, lis);
 
 			// Add the 'active' class to the li element.
 			li.classList.add('active');
@@ -41,4 +28,17 @@ for (let tabGroup of tabGroups) {
 
 	// "Press" the first tab so there always is an active tab.
 	lis[0].querySelector('a').click();
+}
+
+// Function to hide all the content, and deactivate all the tabs.
+function hideAll(tabGroup, lis) {
+	for (let li of lis) {
+
+		// Remove the 'active' class from the li element so it doesn't have the black line.
+		li.classList.remove('active');
+
+		// Get all the divs with the same data-tag as the li element, and add the 'hidden' class.
+		tabGroup.querySelectorAll(`div[data-tab='${li.dataset.tab}']`)
+			.forEach(el => el.classList.add('hidden'));
+	}
 }
